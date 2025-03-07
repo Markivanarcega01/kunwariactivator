436504e7b49462dda0037f0a12b69ff60bc7afe8
Markivanarcega01 <arcegamarkivan@gmail.com>
2025-03-07 22:12:03 +0800

    Update: Initial templates

--- kunwari_activator/settings.py
+++ kunwari_activator/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'kunwariwebpage.apps.KunwariwebpageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
