cce2a06ecfe2fbae0cf2a2bd12e098983ed3792e
Mark Ivan Arcega <Arcegamarkivan@gmail.com>
2025-05-04 02:54:28 +0800

    Update: Made a parser for ChatGPT response

--- static/index.js
+++ static/index.js
@@ -7,14 +7,16 @@ const password = document.getElementById("password");
 const loginSubmit = document.getElementById("login-submit");
 const submitToChatgpt = document.querySelector("#submit-to-chatgpt");
 const generateEpisodes = document.querySelector("#generate-episodes");
-const generateFacilitatorScript = document.querySelector("#generate-facilitator-script");
+const generateFacilitatorScript = document.querySelector(
+  "#generate-facilitator-script"
+);
 const generateContent = document.querySelector("#generate-content");
 const prompt = document.querySelector("#prompt");
 const chatresponse = document.querySelector("#chat_response");
 const generatePptx = document.querySelector("#generate-pptx");
-const downloadPptx = document.querySelector('#download-pptx');
+const downloadPptx = document.querySelector("#download-pptx");
 const message = document.querySelector("#message");
-let fileName = ""
+let fileName = "";
 
 // const adminCredentials = {
 //     username: 'admin',
@@ -29,20 +31,29 @@ let fileName = ""
 // })
 
 //Revise this, change the parameter into string instead of array
-// function format_chatgpt_response(sentences){
-//   //const heading = /^(###.*\n){3}/gm;
-//   const heading = /^(###)/;
-//   const bold = /^\*\*.*\*\*$/;
-//   sentences.forEach(element => {
-//     if(element.match(heading) || element.match(bold)){
-//       cleanData = element.replace(/#+/,"")
-//       addTag = `<h3>${cleanData}</h3>`
-//     }else{
-//       addTag = `<p>${element}</p>`
-//       return addTag
-//     }
-//   });
-// }
+function format_chatgpt_response(paragraph) {
+  const headingOrBold = /^(###)|^\*\*.*\*\*$/;
+  const hrAndDash = /^(<hr>|---)$/;
+
+  /**
+   * Headings = Starts with ### and ends with \n
+   */
+  let sentences = paragraph.split("\n");
+  //console.log(sentences);
+  for (let i = 0; i < sentences.length; i++) {
+    let trimmed_sentence = sentences[i].replace(/[#*]/g,"");
+    if (sentences[i].match(headingOrBold)) {
+      sentences[i] = sentences[i].replace(headingOrBold, `<h3>${trimmed_sentence}</h3>`);
+    } else if (sentences[i] == "") {
+      continue;
+    } else if (sentences[i] == "<hr>" || sentences[i] == "---") {
+      sentences[i] = sentences[i].replace(hrAndDash, `<hr>`);
+    } else {
+      sentences[i] = sentences[i].replace(sentences[i], `<p>${trimmed_sentence}</p>`);
+    }
+  }
+  return sentences.join("\n");
+}
 
 if (registerBtn) {
   registerBtn.addEventListener("click", (e) => {
@@ -58,19 +69,19 @@ if (loginBtn) {
 if (submitToChatgpt) {
   submitToChatgpt.addEventListener("click", async function (e) {
     e.preventDefault();
-    fileName = "lesson_plan.pptx"
+    fileName = "lesson_plan.pptx";
     if (chatresponse.innerHTML != " ") {
-      console.log("ivan too")
+      console.log("ivan too");
       generatePptx.style.display = "block";
       generateEpisodes.style.display = "block";
       //generateContent.style.display = "block";
       //generateFacilitatorScript.style.display = "block";
     }
-    if(generateContent.checkVisibility()){
-      generateContent.style.display = "none"
+    if (generateContent.checkVisibility()) {
+      generateContent.style.display = "none";
     }
-    if(generateFacilitatorScript.checkVisibility()){
-      generateFacilitatorScript.style.display = "none"
+    if (generateFacilitatorScript.checkVisibility()) {
+      generateFacilitatorScript.style.display = "none";
     }
     const response = await fetch("/homepage/chatbot/", {
       method: "POST",
@@ -87,12 +98,50 @@ if (submitToChatgpt) {
       const { done, value } = await reader.read();
       output += new TextDecoder().decode(value);
       //console.log(output)
-      chatresponse.innerHTML = marked.parse(output);
+      //chatresponse.innerHTML = marked.parse(output);
 
       if (done) {
         //let splitted = output.split("\n")
         //console.log(output)
-        //format_chatgpt_response(splitted)
+        chatresponse.innerHTML = format_chatgpt_response(output);
+        console.log(format_chatgpt_response(output));
+        //console.log(output)
+        //console.log(marked.parse(output));
+        return;
+      }
+    }
+  });
+}
+
+if (generateEpisodes) {
+  generateEpisodes.addEventListener("click", async (e) => {
+    e.preventDefault();
+    submitToChatgpt.style.display = "none";
+    generateContent.style.display = "block";
+    fileName = "episodes.pptx";
+    //console.log(chatresponse.textContent)
+    let csrf_token = document.querySelector(
+      "input[name=csrfmiddlewaretoken]"
+    ).value;
+    const response = await fetch("/homepage/generate_episodes/", {
+      method: "POST",
+      headers: {
+        "X-CSRFToken": csrf_token,
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ message: chatresponse.textContent }),
+    });
+    console.log(chatresponse.textContent);
+    let reader = response.body.getReader();
+
+    let output = "";
+
+    while (true) {
+      const { done, value } = await reader.read();
+      output += new TextDecoder().decode(value);
+      chatresponse.innerHTML = marked.parse(output);
+
+      if (done) {
         console.log(marked.parse(output));
         return;
       }
@@ -100,55 +149,24 @@ if (submitToChatgpt) {
   });
 }
 
-
-if(generateEpisodes){
-  generateEpisodes.addEventListener("click", async(e) => {
+if (generateContent) {
+  generateContent.addEventListener("click", async (e) => {
     e.preventDefault();
-    submitToChatgpt.style.display = "none"
-    generateContent.style.display = "block";
-    fileName = "episodes.pptx"
-    //console.log(chatresponse.textContent)
-    let csrf_token = document.querySelector('input[name=csrfmiddlewaretoken]').value
-    const response =await fetch("/homepage/generate_episodes/", {
-      method: "POST",
-      headers: { 
-        "X-CSRFToken": csrf_token,
-        "Content-type": "application/json" },
-      body: JSON.stringify({ message: chatresponse.textContent }),
-    })
-    console.log(chatresponse.textContent)
-    let reader = response.body.getReader();
-
-    let output = "";
-
-    while (true) {
-      const { done, value } = await reader.read();
-      output += new TextDecoder().decode(value);
-      chatresponse.innerHTML = marked.parse(output);
-
-      if (done) {
-        console.log(marked.parse(output));
-        return;
-      }
-    }
-  })
-}
-
-if(generateContent){
-  generateContent.addEventListener("click", async(e) => {
-    e.preventDefault();
-    generateEpisodes.style.display = "none"
+    generateEpisodes.style.display = "none";
     generateFacilitatorScript.style.display = "block";
-    fileName = "content.pptx"
+    fileName = "content.pptx";
     //console.log(chatresponse.textContent)
-    let csrf_token = document.querySelector('input[name=csrfmiddlewaretoken]').value
-    const response =await fetch("/homepage/generate_content/", {
+    let csrf_token = document.querySelector(
+      "input[name=csrfmiddlewaretoken]"
+    ).value;
+    const response = await fetch("/homepage/generate_content/", {
       method: "POST",
-      headers: { 
+      headers: {
         "X-CSRFToken": csrf_token,
-        "Content-type": "application/json" },
+        "Content-type": "application/json",
+      },
       body: JSON.stringify({ message: chatresponse.textContent }),
-    })
+    });
     //console.log(response)
     let reader = response.body.getReader();
 
@@ -164,23 +182,26 @@ if(generateContent){
         return;
       }
     }
-  })
+  });
 }
 
-if(generateFacilitatorScript){
-  generateFacilitatorScript.addEventListener("click", async(e) => {
+if (generateFacilitatorScript) {
+  generateFacilitatorScript.addEventListener("click", async (e) => {
     e.preventDefault();
-    generateContent.style.display = "none"
-    fileName = "facilitator_script.pptx"
+    generateContent.style.display = "none";
+    fileName = "facilitator_script.pptx";
     //console.log(chatresponse.textContent)
-    let csrf_token = document.querySelector('input[name=csrfmiddlewaretoken]').value
-    const response =await fetch("/homepage/generate_facilitator_script/", {
+    let csrf_token = document.querySelector(
+      "input[name=csrfmiddlewaretoken]"
+    ).value;
+    const response = await fetch("/homepage/generate_facilitator_script/", {
       method: "POST",
-      headers: { 
+      headers: {
         "X-CSRFToken": csrf_token,
-        "Content-type": "application/json" },
+        "Content-type": "application/json",
+      },
       body: JSON.stringify({ message: chatresponse.textContent }),
-    })
+    });
     //console.log(response)
     let reader = response.body.getReader();
 
@@ -196,48 +217,54 @@ if(generateFacilitatorScript){
         return;
       }
     }
-  })
+  });
 }
 
-if(generatePptx){
-  generatePptx.addEventListener("click", async(e) => {
+if (generatePptx) {
+  generatePptx.addEventListener("click", async (e) => {
     e.preventDefault();
     //console.log(chatresponse.textContent)
-    let csrf_token = document.querySelector('input[name=csrfmiddlewaretoken]').value
+    let csrf_token = document.querySelector(
+      "input[name=csrfmiddlewaretoken]"
+    ).value;
     await fetch("/homepage/generate_pptx/", {
       method: "POST",
-      headers: { 
+      headers: {
         "X-CSRFToken": csrf_token,
-        "Content-type": "application/json" },
-      body: JSON.stringify({ message: chatresponse.innerHTML, filename:fileName }),
-    }).then(response => response.json())
-      .then(data =>{
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        message: chatresponse.innerHTML,
+        filename: fileName,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
         //downloadPptx.style.display = "block";
-        downloadPptx.disabled = false
-        downloadPptx.setAttribute('filename', data.filename);
+        downloadPptx.disabled = false;
+        downloadPptx.setAttribute("filename", data.filename);
         //message.style.display = "block"
-        message.textContent = `${data.message}`
-        setTimeout(()=>{
+        message.textContent = `${data.message}`;
+        setTimeout(() => {
           //message.style.display = "none"
-          message.textContent = ""
-        },3000)
-      })
-  })
+          message.textContent = "";
+        }, 3000);
+      });
+  });
 }
 
-if(downloadPptx){
-  downloadPptx.addEventListener("click", (e)=>{
-    e.preventDefault()
-    let fileNameAttribute = downloadPptx.getAttribute('filename')
-    const url = `/homepage/download/${fileNameAttribute}`
-    const a = document.createElement('a');
+if (downloadPptx) {
+  downloadPptx.addEventListener("click", (e) => {
+    e.preventDefault();
+    let fileNameAttribute = downloadPptx.getAttribute("filename");
+    const url = `/homepage/download/${fileNameAttribute}`;
+    const a = document.createElement("a");
     a.href = url;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
-    downloadPptx.removeAttribute('filename')
+    downloadPptx.removeAttribute("filename");
     //downloadPptx.style.display = "none"
-    downloadPptx.disabled = true
-  })
+    downloadPptx.disabled = true;
+  });
 }
-
